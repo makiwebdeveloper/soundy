b4d1407e23fd4d602551070aa8badb265a43e836
makiwebdeveloper <makiwebdeveloper@gmail.com>
2023-12-27 23:17:59 +0100

    change auth from client component to server

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,16 +1,10 @@
-"use client";
-
 import { PropsWithChildren } from "react";
-import { useMount } from "@/hooks/use-mount";
-import Loader from "@/components/loader";
 import { ModeToggle } from "@/components/mode-toggle";
 
 export default function AuthLayout({ children }: PropsWithChildren) {
-  const isMounted = useMount();
-
   return (
     <main className="w-full h-screen flex-center">
-      {isMounted ? children : <Loader />}
+      {children}
       <div className="absolute top-5 right-5 sm:top-10 sm:right-10">
         <ModeToggle noText />
       </div>
--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { SignIn } from "@clerk/nextjs";
 import { Metadata } from "next";
 
