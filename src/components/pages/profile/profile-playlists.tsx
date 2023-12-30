88b9e858d14f5df852ba2af603e6b564b269f552
makiwebdeveloper <makiwebdeveloper@gmail.com>
2023-12-30 12:12:21 +0100

    playlist image fix

--- src/components/pages/profile/profile-playlists.tsx
+++ src/components/pages/profile/profile-playlists.tsx
@@ -26,24 +26,28 @@ export default function ProfilePlaylists({ playlists, profile }: Props) {
         </Link>
       </div>
       <div className="h-[90px] flex gap-[22px] mx-2 mt-2">
-        {playlists.map((playlist) => (
-          <Link
-            key={playlist.id}
-            href={`/profiles/${profile.id}/playlists/${playlist.id}`}
-            className={cn(
-              `relative w-[80px] h-[80px] rounded-md z-[0] after:transition-all before:transition-all`,
-              "after:content-[''] after:z-[-10] after:absolute after:top-[5px] after:left-[5px] after:rounded-md after:w-full after:h-full after:bg-white/50 dark:after:bg-black/80 hover:after:left-[8px] hover:after:top-[8px] after:duration-500",
-              "before:content-[''] after:z-[-20] before:absolute before:top-[10px] before:left-[10px] before:rounded-md before:w-full before:h-full before:bg-white/20 dark:before:bg-black/50 hover:before:left-[16px] hover:before:top-[16px] before:duration-500"
-            )}
-          >
-            <Image
-              fill
-              src={playlist.tracks[0].imageUrl}
-              alt={playlist.title}
-              className="object-cover rounded-md bg-white/20 dark:bg-black/20"
-            />
-          </Link>
-        ))}
+        {playlists.map((playlist) => {
+          if (!playlist.tracks[0]) return null;
+
+          return (
+            <Link
+              key={playlist.id}
+              href={`/profiles/${profile.id}/playlists/${playlist.id}`}
+              className={cn(
+                `relative w-[80px] h-[80px] rounded-md z-[0] after:transition-all before:transition-all`,
+                "after:content-[''] after:z-[-10] after:absolute after:top-[5px] after:left-[5px] after:rounded-md after:w-full after:h-full after:bg-white/50 dark:after:bg-black/80 hover:after:left-[8px] hover:after:top-[8px] after:duration-500",
+                "before:content-[''] after:z-[-20] before:absolute before:top-[10px] before:left-[10px] before:rounded-md before:w-full before:h-full before:bg-white/20 dark:before:bg-black/50 hover:before:left-[16px] hover:before:top-[16px] before:duration-500"
+              )}
+            >
+              <Image
+                fill
+                src={playlist.tracks[0].imageUrl || ""}
+                alt={playlist.title}
+                className="object-cover rounded-md bg-white/20 dark:bg-black/20"
+              />
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
