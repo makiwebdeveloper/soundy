050c0d2d2b437559df8d3160456e6770cbeef2cc
makiwebdeveloper <makiwebdeveloper@gmail.com>
2023-12-30 12:14:59 +0100

    auth redirect to home

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -8,5 +8,5 @@ export const metadata: Metadata = {
 };
 
 export default function Page() {
-  return <SignIn />;
+  return <SignIn redirectUrl="/" />;
 }
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -8,5 +8,5 @@ export const metadata: Metadata = {
 };
 
 export default function Page() {
-  return <SignUp />;
+  return <SignUp redirectUrl="/" />;
 }
